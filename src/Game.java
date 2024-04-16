4d1df81a78758219b0536a93f8e52fa7059ef4a8
DenisLazarian <denis.emanuel.lazarian@gmail.com>
2024-04-16 19:17:33 +0200

    realizando la funcion isValidFrom.

--- src/Game.java
+++ src/Game.java
@@ -26,14 +26,50 @@ public class Game {
     }
 
     public boolean isValidFrom(Position position) {
-        return false;
+        if(isColorTurn(Player.WHITE)) {
+            return isValidByPieceColor(position, WHITE_DIRECTIONS);
+        } else {
+            return isValidByPieceColor(position, BLACK_DIRECTIONS);
+        }
     }
 
+    public boolean isValidByPieceColor(Position p, Direction[] c){
+        boolean t0;
+        boolean t1;
+        if(isColorTurn(Player.WHITE)){
+            t0 = board.isBlack(c[0].apply(p));
+            t1 = board.isBlack(c[1].apply(p));
+        }else{
+            t0 = board.isWhite(c[0].apply(p));
+            t1 = board.isWhite(c[1].apply(p));
+        }
+
+        return  !board.isEmpty(p) && !isColor(p) &&
+                ((board.isEmpty(c[0].apply(p)) ||
+                  board.isEmpty(c[1].apply(p))) ||
+                (t0 && board.isEmpty(c[0].apply(c[0].apply(p))) ||
+                  t1 && board.isEmpty(c[1].apply(c[1].apply(p)))));
+    }
+
+
+
+    public boolean isColor(Position p){
+        if(isColorTurn(Player.WHITE)) return board.isBlack(p);
+        else return board.isWhite(p);
+    }
 
 
     // Assumes validFrom is a valid starting position
     public boolean isValidTo(Position validFrom, Position to) {
         return false;
+//        return isValidFrom(validFrom) &&
+//                board.isEmpty(to) &&
+//                validFrom.sameDiagonalAs(to) &&
+//                Position.distance(validFrom,to) == 2 ||
+//                isValidFrom(validFrom) &&
+//                isColorByTurn(validFrom) &&
+//                validFrom.sameDiagonalAs(to) &&
+//                Position.distance(validFrom,to) == 4;
     }
 
     private boolean isColorByTurn(Position validFrom) {
@@ -47,6 +83,15 @@ public class Game {
 
     // Assumes both positions are valid
     public Move move(Position validFrom, Position validTo) {
+        if(isValidFrom(validFrom) && isValidTo(validFrom, validTo)){
+            Move move;
+            if(Position.distance(validFrom, validTo) == 4)
+                move = new Move(validFrom, Position.middle(validFrom, validTo),validTo);
+            else move = new Move(validFrom, null, validTo);
+
+            updateBoard(move);
+            return move;
+        }
         return null;
     }
 
